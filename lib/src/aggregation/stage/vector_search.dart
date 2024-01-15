b99bcae04985519c94139f6a83c30664ae263f23
Giorgio Franceschetti <giorgiofran@gmail.com>
2024-01-15 21:28:54 +0100

    Dev 3.11

--- lib/src/aggregation/stage/vector_search.dart
+++ lib/src/aggregation/stage/vector_search.dart
@@ -1,9 +1,7 @@
-import 'package:mongo_db_query/mongo_db_query.dart';
-import 'package:mongo_db_query/src/base/operator_expression.dart';
-
 import '../../base/common/document_types.dart';
 import '../../base/common/operators_def.dart';
 import '../../base/map_expression.dart';
+import '../../query_expression/filter_expression.dart';
 import '../../query_expression/query_expression.dart';
 import '../base/aggregation_stage.dart';
 
